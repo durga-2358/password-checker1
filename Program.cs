5dc2b69db7773dc71839fceacf34ad7648b55e6c
Durgadevi Umakhanth <d.umakhanth3@gmail.com>
2025-10-22 15:53:58 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the password checker");
+            Console.WriteLine("the password has to be 13 characters long and has to contain '!' or '$' or '.'");
+            Console.WriteLine("enter your password to check if it is vaild");
+
+            string password = "";
+            bool isvalid = false;
+            while (!isvalid)
+            {
+                password = Console.ReadLine();
+
+                bool hasSpecialChar = password.Contains(".") || password.Contains("!") || password.Contains("$");
+                bool is13charlong = password.Length == 13;
+
+
+                if (hasSpecialChar && is13charlong)
+                {
+                    Console.WriteLine("the password you have entered is vaild");
+                }
+                else
+                {
+                    Console.WriteLine(" the password you have entered is not valid so please try again");
+                }
+            }
+
+            Console.ReadLine();
+
+
+               
+
+
+         }
+    }
+}
